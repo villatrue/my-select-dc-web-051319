7ed71657454488baa473d7184ed11377d5652666
Sean Carter <scarter0313@gmail.com>
2019-05-09 02:30:47 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -12,14 +12,14 @@ def my_select(array)
 end
 
 
-def my_select(array)
-  i = 0
-  select = []
-  while i < array.length
-    if yield(array[i])
-      select << array[i]
-    end
-    i+=1
-  end
-  select
-end
+# def my_select(array)
+#   i = 0
+#   select = []
+#   while i < array.length
+#     if yield(array[i])
+#       select << array[i]
+#     end
+#     i+=1
+#   end
+#   select
+# end
