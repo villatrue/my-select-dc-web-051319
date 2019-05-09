755ad732559f5535b40d2a9493e1d0db60d3f839
Sean Carter <scarter0313@gmail.com>
2019-05-09 02:28:28 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,24 +1,24 @@
 
-# def my_select(array)
-#   i = 0
-#   select = []
-#   while i < array.length
-#   if yield(array[i])
-#     select << yield(array[i])
-#     i+=1
-#   end
-#   select
-# end
-
-
 def my_select(array)
   i = 0
   select = []
   while i < array.length
-    if yield(array[i])
-      select << array[i]
-    end
+  if yield(array[i])
+    select << yield array[i]
     i+=1
   end
   select
 end
+
+
+# def my_select(array)
+#   i = 0
+#   select = []
+#   while i < array.length
+#     if yield(array[i])
+#       select << array[i]
+#     end
+#     i+=1
+#   end
+#   select
+# end
